8c3a73fd6ca7d005b9f47cdcbc27dc674a9ba73a
Kazim Ahmad <bitchy_kazim@Kazims-MacBook-Pro.local>
2026-04-18 18:48:24 +0100

    project

---
+++ Bo/Bo/BoApp.swift
@@ -0,0 +1,32 @@
+//
+//  BoApp.swift
+//  Bo
+//
+//  Created by Kazim Ahmad on 18/04/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Bo/BoUITests/BoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BoUITestsLaunchTests.swift
+//  BoUITests
+//
+//  Created by Kazim Ahmad on 18/04/2026.
+//
+
+import XCTest
+
+final class BoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
